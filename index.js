dc9edfcd04263cd8fa74fac9f9957332ce69d3b9
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-08-08 11:45:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express"); // Used to set up a server
+const cors = require("cors"); // Used to prevent errors when working locally
+const app = express(); // Initialize express as an app variable
+app.set("port", process.env.PORT || 8081); // Set the port
+app.use(express.json()); // Enable the server to handle JSON requests
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome" });
+});
+
+app.use("/users", userRoute);
+app.use("/products", productRoute);
+app.use("/categories", categoryRoute);
+app.use("/orders", orderRoute);
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening for calls on port ${app.get("port")}`);
+  console.log("Press Ctrl+C to exit server");
+});
---
+++ routes/productsRoute.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+
+module.exports = router;
