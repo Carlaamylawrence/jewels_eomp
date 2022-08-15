f1c65c85262e575a974795a17b94b0ab21b09247
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-08-15 16:15:39 +0200

    added cors fix

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ app.set("port", process.env.PORT || 8081);
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to jewels" });
 });
@@ -19,3 +25,9 @@ app.listen(app.get("port"), () => {
   console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
 });
+
+module.exports = {
+  devServer: {
+    Proxy: "*",
+  },
+};
